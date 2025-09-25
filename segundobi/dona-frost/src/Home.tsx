9f48c4014461d2a359f123096c9f7f9211147da5
Sandrolaxx <sandro--ramos@hotmail.com>
2025-09-25 16:27:58 -0300

    add primeiros exemplos do projeto + reset css

---
+++ segundobi/dona-frost/src/Home.tsx
@@ -0,0 +1,17 @@
+import "./Home.css";
+import "./utility.css";
+
+export function Home() {
+    return (
+        <div className="container">
+            <a href="">Home</a>
+            <h1>
+                Teste show
+            </h1>
+            <li>TEste222</li>
+            <button>
+                Teste
+            </button>
+        </div>
+    )
+}
